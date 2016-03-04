10e394553aa32c4e85996d862932fa1526f3ccb4
Godfrey Chiu <ggwc82@gmail.com>
2016-03-04 22:07:01 +0000

    WIP Written failing tests for average rating feature

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -13,4 +13,22 @@ feature 'reviewing' do
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content('so so')
   end
+
+  def leave_review(thoughts, rating)
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in 'Thoughts', with: thoughts
+    select rating, from: 'Rating'
+    click_button 'Leave Review'
+  end
+
+  scenario 'displays an average rating for all reviews' do
+    sign_up_and_in('user1@test.com')
+    create_restaurant('KFC')
+    leave_review('So so', '3')
+    click_link 'Sign out'
+    sign_up_and_in('user2@test.com')
+    leave_review('Great', '5')
+    expect(page).to have_content('Average rating: 4')
+  end
 end
